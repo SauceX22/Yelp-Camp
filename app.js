ced15e901d1f13daa14fdfe8678ba50f86e48ef2
SauceX22 <mahmoudzak4@gmail.com>
2022-07-04 17:56:36 +0300

    Fixed start script

--- app.js
+++ app.js
@@ -161,6 +161,8 @@ app.use((err, req, res, next) => {
     console.log(err)
 })
 
-app.listen(3000, () => {
-    console.log('Serving on PORT: 3000!');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Serving on PORT: ${port}!`);
 })
